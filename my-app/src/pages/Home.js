afe080df1b914277e9f2a2896935c848c070088c
codeawayjustcodeaway <112389416+Markojuuh@users.noreply.github.com>
2025-05-10 21:26:23 +0200

    satt in import for events

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import FeaturedEvents from '../components/FeaturedEvents';
 
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <main>
+      <h1>Home</h1>
+        {/*Dette skal render de 4 featured eventcards */}
+      <FeaturedEvents />
+    </main>
+  );
 }
